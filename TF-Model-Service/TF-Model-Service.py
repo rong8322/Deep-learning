3700cd771fec7568367aae44c2749086b765d855
rong8322 <rong8322@gmail.com>
2018-01-18 09:49:02 +0800

    Add files via upload

---
+++ TF-Model-Service/TF-Model-Service.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+import model
+import cv2 as cv
+import numpy as np
+
+app = Flask(__name__)
+
+server = model.DefaultModelServer()
+
+@app.route('/')
+def hello_world():
+    return 'Yo!'
+
+
+@app.route('/single-digit', methods=['POST'])
+def single_digit():
+    print('data length', len(request.data))
+    print(type(request.data))
+    frombuffer = np.frombuffer(request.data, dtype='uint8')
+    print(type(frombuffer), frombuffer.shape)
+    q = cv.imdecode(np.frombuffer(request.data, dtype='uint8'), cv.IMREAD_COLOR)
+    print(type(q), q.shape)
+    return str(server.serve(q))
+	# return 1
+
+
+if __name__ == '__main__':
+	app.run()
---
+++ TF-Model-Service/model.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+# change it to your own
+graph_path = 'D:\Python\CNN\Models\MNIST_4Conv_32x32.ckpt.meta'
+weights_path = 'D:\Python\CNN\Models\MNIST_4Conv_32x32.ckpt'
+
+
+class DefaultModelServer():
+	def __init__(self):
+		self.sess = tf.Session()
+		self.new_saver = tf.train.import_meta_graph(graph_path)
+		self.new_saver.restore(self.sess, weights_path)
+		self.test_prediction = tf.get_default_graph().get_tensor_by_name('test/single_prediction:0')
+		self.single_input = tf.get_default_graph().get_tensor_by_name('test/single_input:0')
+#       print('DefaultModelServer is up')
+
+	def compute(self, inputData):
+		'''
+		:param inputData: a ndarray with shape (1, 32, 32, 1), representing 1 grayscaled image
+		:return: the softmaxed result
+		'''
+		return self.sess.run(self.test_prediction, feed_dict={self.single_input: inputData})
+
+	def transform_data(self, image):
+		# :param image: ndarray
+		# the shape of image should be (32, 32, 1)
+		if image.shape[2] != 1:
+			image = tf.image.rgb_to_grayscale(image)
+		image = tf.image.resize_images(image, (32, 32))
+		print('transform', type(image))
+		ret = [image.eval(session=self.sess)]  # todo: should to grayscale and resize outside and use OpenCV maybe?
+		print('transform', type(ret[0]))
+		return ret
+
+	def serve(self, image):
+		ret = self.compute(self.transform_data(image))
+		print('serve', type(ret))
+		return ret
+
+
+# Test
+if __name__ == '__main__':
+    server = DefaultModelServer()
+    for i in range(0,54):
+        im = cv.imread('D:\\Python\\CNN\\MNIST\\testimage\\number\\' +str(i) + ".jpg", cv.IMREAD_COLOR)
+        result = server.serve(im)
+        print(str(result))
+        print(np.argmax(result))
+        plt.imshow(im)
+        plt.show()
